18e18ae4086e5cf9f3e9ac5a36b9f40e802ba9f5
francisXKF <xkf1273@gmail.com>
2018-06-05 15:06:01 +0800

    add echarts map

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Zhangyr's Pages
 
 ### collaborators coffeyXu
+
+### *Prov.json
+* 0 未出账
+* 1 出账中第一阶段
+* 2 出账中第二阶段
+* 3 出账中第三阶段
+...
+* 10 出账完成
---
+++ style/jzjsProv.css
@@ -0,0 +1,12 @@
+var g_state_color = [
+"#FFFFFF",
+"#00FF00",
+"#FFFF00",
+"#FFFF00",
+"#FFFF00",
+"#FFFF00",
+"#FFFF00",
+"#FFFF00",
+"#FFFF00",
+"#FF0000"
+];
