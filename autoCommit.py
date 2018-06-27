8d9abb598263e6bb8689d545745b0eba2c5d856e
francisXKF <xkf1273@gmail.com>
2018-06-27 14:40:23 +0800

    update provInfo

--- autoCommit.py
+++ autoCommit.py
@@ -3,12 +3,15 @@
 import os
 import time
 
+print 'start git add'
 os.system("git add -A")
 time.sleep(3)
 
+print 'start git commit'
 os.system("git commit -a -m \"update provInfo\"")
 time.sleep(3)
 
+print 'start git push'
 os.system("git push")
 
 print "update ok~"
