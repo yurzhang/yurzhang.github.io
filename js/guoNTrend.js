57ab9f6fb81617c3d5c5fb8b478c592b57af77f7
francisXKF <xkf1273@gmail.com>
2018-08-28 16:44:06 +0800

    update provInfo

--- js/guoNTrend.js
+++ js/guoNTrend.js
@@ -3,16 +3,19 @@ function echartsDraw(jsonName, mapId){
   var provDateSettings = new Array();
   console.log(jsonName);
   $.get("../data/"+jsonName).done(function(data){ 
-    console.log("get data");
-    $.each(data,function(infoIndex,info){ 
+    console.log("get data 23333");
+    $.each(data, function(infoIndex,info){ 
         provDate.push({
           roamAndMsi: info.roam+','+info.msi,
           value: Number(info.value)
         });
     });
 
+    console.log(provDate);
     var provDateOrderd = Object.keys(provDate).sort(function(a, b){return provDate[a] - provDate[b]});
     console.log(provDateOrderd);
+
+    console.log(".....................................................");
     // echarts.registerMap('china', data);
     var bmapCharts = echarts.init(document.getElementById(mapId));
     // 初始化地图
