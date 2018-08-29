25823859f9a0a5ac725b8013ab56d9f257dcfc6f
francisXKF <xkf1273@gmail.com>
2018-08-29 15:35:50 +0800

    update provInfo

--- js/guoJTrend.js
+++ js/guoJTrend.js
@@ -204,7 +204,7 @@ function guoJEchartsDraw(jsonName, mapId){
           zoom: 1.23,
           label: {
               normal:{//是图形在默认状态下的样式
-                  show : true
+                  show : false
               },
               emphasis: {
                   show: false
