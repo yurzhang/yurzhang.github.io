8810420725f2a42a466243c0b7255c80a9076182
francisXKF <xkf1273@gmail.com>
2018-08-29 16:32:17 +0800

    update provInfo

--- js/guoJTrend.js
+++ js/guoJTrend.js
@@ -49,9 +49,6 @@ function guoJEchartsDraw(jsonName, mapId){
     var worldNameShowData = new Array();
     // 处理点显示
     nationSetTop10.forEach(function(item, i) {
-      console.log(item);
-      console.log(gWorldName[item]);
-      console.log("..........");
       worldNameShowData.push({        // 选中的区域
         name: gWorldName[item],
         // selected: true,
@@ -76,37 +73,6 @@ function guoJEchartsDraw(jsonName, mapId){
         // }
       });
     });
-    // var worldNameShowData = [{        // 选中的区域
-    //           name: 'China',
-    //           selected: true,
-    //           itemStyle: {   // 高亮时候的样式
-    //             emphasis: {
-    //                 areaColor: '#7d7d7d'
-    //             }
-    //           },
-    //          label: {
-    //               emphasis: {
-    //                  // show: false
-    //                   show: true
-    //              }
-    //           }
-    //         },
-    //         {        // 选中的区域
-    //           name: 'United States',
-    //           selected: true,
-    //           itemStyle: {   // 高亮时候的样式
-    //             emphasis: {
-    //                 areaColor: '#7d7d7d'
-    //             }
-    //           },
-    //          label: {
-    //               emphasis: {
-    //                  // show: false
-    //                   show: true
-    //              }
-    //           }
-    //         },
-    //         ];
 
     var series = [];
       [['10', nationchargedUnitsTop10]].forEach(function (item, i) {
@@ -223,7 +189,7 @@ function guoJEchartsDraw(jsonName, mapId){
       tooltip : {
           trigger: 'item',
           formatter: function (v) {
-            // console.log(v);
+            console.log(v);
             if (v.seriesName == 'mapContentX') {
               return allnationUnits[v.name];
             }
