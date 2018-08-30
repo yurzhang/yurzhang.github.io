0e055e5167b531dfb25c2958831f015d727b8800
francisXKF <xkf1273@gmail.com>
2018-08-30 18:35:35 +0800

    update provInfo

--- js/overviewTrend.js
+++ js/overviewTrend.js
@@ -26,7 +26,7 @@ function overviewEchartsDraw(jsonName, mapId){
     };
     option = {
         title : {
-            text: '各业务话单量占比',
+            text: '本月各业务话单量占比',
             x:'center'
         },
         tooltip : {
@@ -64,6 +64,96 @@ function overviewEchartsDraw(jsonName, mapId){
             }
         ]
     };
+
+    bmapCharts.clear();
+    bmapCharts.setOption(option);
+  });
+}
+
+function overviewTrendEchartsDraw(jsonName, mapId){
+  var provDate = new Array();
+  var provDateSettings = new Array();
+
+  var provSetTop10 = new Set();
+
+  console.log(jsonName);
+  $.get("../data/"+jsonName).done(function(data) { 
+    console.log("get data overview");
+    $.each(data, function(infoIndex, info) { 
+        provDate.push({
+          provId: info.roam+','+info.msi,
+          value: Number(info.value)
+        });
+    });
+
+    var bmapCharts = echarts.init(document.getElementById(mapId));
+    // 初始化地图
+    console.log(mapId);
+    console.log("initail map ok");
+
+    option = {
+        title: {
+            text: '近六个月各业务话单量趋势'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['网间','增值','国内漫游','国际漫游','其它']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['二','三','四','五','六','七','八']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name:'网间',
+                type:'line',
+                stack: '总量',
+                data:[26636824701, 32723875567, 32111190698, 32815388288, 31786529384, 32788038370, 32788038370]
+            },
+            {
+                name:'增值',
+                type:'line',
+                stack: '总量',
+                data:[3414460581, 2727134216, 2815955134, 2354935076, 3304982359, 3183864451, 3183864451]
+            },
+            {
+                name:'国内漫游',
+                type:'line',
+                stack: '总量',
+                data:[78994334880, 73669733921, 74254733988, 78637955496, 79321450833, 89890991021, 89890991021]
+            },
+            {
+                name:'国际漫游',
+                type:'line',
+                stack: '总量',
+                data:[0, 0, 0, 0, 0, 723636143, 723636143]
+            },
+            {
+                name:'其他',
+                type:'line',
+                stack: '总量',
+                data:[6569830025, 8483216783, 9412479717, 9609853507, 9609368654, 9518070513, 9518070513]
+            }
+        ]
+    };
+
 
     bmapCharts.clear();
     bmapCharts.setOption(option);
